562ce2fdea67797d388a0e9e50d1931f4618612a
P16190097 <mayoze.jm@googlemail.com>
2020-05-11 16:54:13 +0100

    refactored init files

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-
-const botconfig = require("./botconfig.json");
-
-
 import Discord from 'discord.js';
+import config from './botconfig.json';
 
 const client = new Discord.Client({disableEveryone: true});
 
 // TODO: get token from discord client to add here
 client.login(process.env.CLIENT_TOKEN);
 
-client.on("ready", async () => {
+client.on('ready', async () => {
   console.log(`${client.user.username} is online!`);
 });
 
-client.on("message", async message => {
+client.on('message', async message => {
   if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
+  if(message.channel.type === 'dm') return;
 
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
+  const prefix = config.prefix;
+  const messageArray = message.content.split(' ');
+  const cmd = messageArray[0];
+  //const args = messageArray.slice(1);
 
   if(message.content.includes('bee')) {
     return message.channel.send('!play https://www.youtube.com/watch?v=MADvxFXWvwE');
   }
 
   if (cmd === `${prefix}hello`) {
-      return message.channel.send("hello!");
+      return message.channel.send('hello!');
   }
 
 });
